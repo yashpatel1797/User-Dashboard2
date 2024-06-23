e6e445a96d9182a15121c83a0c8ff91d56e763aa
yashpatel1797 <yes.g.patel@gmail.com>
2024-06-24 02:13:42 +0530

    card view improvement

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <div class="flex flex-col sm:flex-row items-start sm:items-center justify-between p-4 border-2 border-grey rounded cursor-pointer hover:bg-gray-100">
       <div class="flex items-center">
         <div class="w-12 h-12 rounded-full mr-4 flex-shrink-0"></div>
@@ -14,7 +14,23 @@
         {{ user.email }}
       </a>
     </div>
-  </template>
+  </template> -->
+
+  <template>
+  <div class="flex items-center justify-between p-4 border-b cursor-pointer hover:bg-gray-50">
+    <div class="flex items-center">
+      <div class="w-10 h-10 bg-gray-300 rounded-full mr-4 flex-shrink-0"></div>
+      <div>
+        <h3 class="text-base font-medium text-gray-900">{{ user.name }}</h3>
+        <p class="text-sm text-gray-500">{{ user.username }}</p>
+      </div>
+    </div>
+    <a :href="`mailto:${user.email}`" class="text-sm text-blue-500 hover:underline">
+      {{ user.email }}
+    </a>
+  </div>
+</template>
+
   
   <script>
   export default {
